02e8e80371b34dc8c2dab8b56610e90f536a6d6a
mglass <mark.glass@cgifederal.com>
2021-02-08 11:25:28 -0500

    clean up

--- src/main/java/org/demo/App.java
+++ src/main/java/org/demo/App.java
@@ -1,20 +1,25 @@
 package org.demo;
 
 import java.io.FileInputStream;
-import java.io.*;
-
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
-import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
-import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.ss.usermodel.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 
-import static org.apache.poi.ss.usermodel.CellType.*;
-import com.itextpdf.text.*;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -30,82 +35,41 @@ public class App
         App app = new App();
 
         try {
-            app.convertDocXToPdf();
-            app.convertXslToPdf();
-            app.convertXslXToPdf();
-           // app.convertOdsToPdf();
-        } catch (IOException | DocumentException e) {
+            //app.convertDocToPdf("example/resume.doc", "example/out/resume_doc.pdf");
+            app.convertDocxToPdf("example/resume.docx", "example/out/resume_docx.pdf");
+            app.convertXslToPdf("example/test.xls", "example/out/test_xsl.pdf");
+            app.convertXslxToPdf("example/test.xlsx", "example/out/test_xslx.pdf");
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("DONE!");
     }
 
-    private void convertDocXToPdf() throws IOException {
-            InputStream docFile = new FileInputStream("example/resume.docx");
-            XWPFDocument doc = new XWPFDocument(docFile);
+    private void convertDocToPdf(String sourcePath, String outputPath) throws IOException {
+            InputStream docFile = new FileInputStream(sourcePath);
+            HWPFDocument doc = new HWPFDocument(docFile);
             PdfOptions pdfOptions = PdfOptions.create();
-            OutputStream out = new FileOutputStream("example/out/resume.pdf");
-            PdfConverter.getInstance().convert(doc,out, pdfOptions);
+            OutputStream out = new FileOutputStream(outputPath);
+//            PdfWriter.getInstance(doc, out);
+//            //PdfConverter.getInstance().convert(doc,out, pdfOptions); openSagres converter does not work
+
             doc.close();
             out.close();
     }
 
-    private void convertDocToPdf() throws IOException {
-        InputStream docFile = new FileInputStream("example/resume.docx");
+    private void convertDocxToPdf(String sourcePath, String outputPath) throws IOException {
+        InputStream docFile = new FileInputStream(sourcePath);
         XWPFDocument doc = new XWPFDocument(docFile);
         PdfOptions pdfOptions = PdfOptions.create();
-        OutputStream out = new FileOutputStream("example/out/resume.pdf");
+        OutputStream out = new FileOutputStream(outputPath);
         PdfConverter.getInstance().convert(doc,out, pdfOptions);
+
         doc.close();
         out.close();
     }
 
-    private void convertXslXToPdf() throws IOException, DocumentException {
-        FileInputStream input_document = new FileInputStream("example/test.xlsx");
-        // Read workbook into HSSFWorkbook
-        XSSFWorkbook my_xls_workbook = new XSSFWorkbook(input_document);
-        // Read worksheet into HSSFSheet
-        XSSFSheet my_worksheet = my_xls_workbook.getSheetAt(0);
-        // To iterate over the rows
-        Iterator<Row> rowIterator = my_worksheet.iterator();
-        //We will create output PDF document objects at this point
-        Document iText_xls_2_pdf = new Document();
-        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream("example/out/test_xslx.pdf"));
-        iText_xls_2_pdf.open();
-        //we have two columns in the Excel sheet, so we create a PDF table with two columns
-        //Note: There are ways to make this dynamic in nature, if you want to.
-        PdfPTable my_table = new PdfPTable(2);
-        //We will use the object below to dynamically add new data to the table
-        PdfPCell table_cell;
-        //Loop through rows.
-        while(rowIterator.hasNext()) {
-            Row row = rowIterator.next();
-            Iterator<Cell> cellIterator = row.cellIterator();
-            while(cellIterator.hasNext()) {
-                Cell cell = cellIterator.next(); //Fetch CELL
-                switch(cell.getCellType()) { //Identify CELL type
-                    //you need to add more code here based on
-                    //your requirement / transformations
-                    case STRING:
-                        //Push the data from Excel to PDF Cell
-                        table_cell=new PdfPCell(new Phrase(cell.getStringCellValue()));
-                        //feel free to move the code below to suit to your needs
-                        my_table.addCell(table_cell);
-                        break;
-                }
-                //next line
-            }
-
-        }
-        //Finally add the table to PDF document
-        iText_xls_2_pdf.add(my_table);
-        iText_xls_2_pdf.close();
-        //we created our pdf file..
-        input_document.close(); //close xls
-    }
-
-    private void convertXslToPdf() throws IOException, DocumentException {
-        FileInputStream input_document = new FileInputStream("example/test.xls");
+    private void convertXslToPdf(String sourcePath, String outputPath) throws IOException, DocumentException {
+        FileInputStream input_document = new FileInputStream(sourcePath);
         // Read workbook into HSSFWorkbook
         HSSFWorkbook my_xls_workbook = new HSSFWorkbook(input_document);
         // Read worksheet into HSSFSheet
@@ -114,7 +78,7 @@ public class App
         Iterator<Row> rowIterator = my_worksheet.iterator();
         //We will create output PDF document objects at this point
         Document iText_xls_2_pdf = new Document();
-        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream("example/out/test_xsl.pdf"));
+        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(outputPath));
         iText_xls_2_pdf.open();
         //we have two columns in the Excel sheet, so we create a PDF table with two columns
         //Note: There are ways to make this dynamic in nature, if you want to.
@@ -148,8 +112,8 @@ public class App
         input_document.close(); //close xls
     }
 
-    private void convertOdsToPdf() throws IOException, DocumentException {
-        FileInputStream input_document = new FileInputStream("example/test.ods");
+    private void convertXslxToPdf(String sourcePath, String outputPath) throws IOException, DocumentException {
+        FileInputStream input_document = new FileInputStream(sourcePath);
         // Read workbook into HSSFWorkbook
         XSSFWorkbook my_xls_workbook = new XSSFWorkbook(input_document);
         // Read worksheet into HSSFSheet
@@ -158,7 +122,7 @@ public class App
         Iterator<Row> rowIterator = my_worksheet.iterator();
         //We will create output PDF document objects at this point
         Document iText_xls_2_pdf = new Document();
-        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream("example/out/test_ods.pdf"));
+        PdfWriter.getInstance(iText_xls_2_pdf, new FileOutputStream(outputPath));
         iText_xls_2_pdf.open();
         //we have two columns in the Excel sheet, so we create a PDF table with two columns
         //Note: There are ways to make this dynamic in nature, if you want to.
